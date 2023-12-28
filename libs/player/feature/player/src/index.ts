ebd3dc7400938cf3910d4f80b5e1b0fdafc2ebb8
Victor Avila <vicav99@gmail.com>
2023-12-27 22:09:13 -0700

    chore: generate player feature

---
+++ libs/player/feature/player/src/index.ts
@@ -0,0 +1,1 @@
+export { PlayerFeature } from './lib/player';
---
+++ libs/player/feature/player/src/lib/player.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable-next-line */
+export interface PlayerFeatureProps {}
+
+export function PlayerFeature(props: PlayerFeatureProps) {
+  return (
+    <div>
+      <h1>Welcome to PlayerFeature!</h1>
+    </div>
+  );
+}
