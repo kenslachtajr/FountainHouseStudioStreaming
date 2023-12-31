eaf77e9a0d9b892bd36f17b1e5cbcf61dc737711
Victor Avila <vicav99@gmail.com>
2023-12-30 23:55:03 -0700

    feat: first pass pwa

--- apps/music-player/next.config.js
+++ apps/music-player/next.config.js
@@ -15,11 +15,24 @@ const nextConfig = {
   images: {
     domains: ['picsum.photos', 'loremflickr.com', 'images.squarespace-cdn.com'],
   },
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true, // Enable SWC minification for improved performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development', // Remove console.log in production
+  },
 };
+
+const withPWA = require('next-pwa')({
+  dest: 'public', // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
   withNx,
+  withPWA,
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
